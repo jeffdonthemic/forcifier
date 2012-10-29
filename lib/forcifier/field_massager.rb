7275b987ae00e7512d0ee691541a3a1609e3efbf
Jeff Douglas <jeff@jeffdouglas.com>
2012-10-29 06:02:05 -0400

    version bump

--- lib/forcifier/field_massager.rb
+++ lib/forcifier/field_massager.rb
@@ -1,35 +1,33 @@
 module Forcifier
-
 	class FieldMassager
 
 		STANDARD_FIELDS = %w(id isdeleted name createddate createdbyid lastmodifieddate lastmodifiedbyid systemmodstamp lastactivitydate)
 
-	  #
-	  # Adds "__c" to all non-standard salesforce.com fields
-	  # * *Args*    :
-	  #   - fields -> comma separated list of fields "id,name,wins"
-	  # * *Returns* :
-	  #   - comma separated list of fields "id,name,wins__c"
-	  # * *Raises* :
-	  #   - ++ ->
-	  #
+		#
+		# Adds "__c" to all non-standard salesforce.com fields
+		# * *Args*    :
+		#   - fields -> comma separated list of fields "id,name,wins"
+		# * *Returns* :
+		#   - comma separated list of fields "id,name,wins__c"
+		# * *Raises* :
+		#   - ++ ->
+		#
 		def self.enforce_fields(fields)
 			fields.downcase.split(',').enforce(STANDARD_FIELDS).join(',')
 		end
 
-	  #
-	  # Removes "__c" from all fields fields
-	  # * *Args*    :
-	  #   - fields -> comma separated list of fields "id,name,wins__c"
-	  # * *Returns* :
-	  #   - comma separated list of fields "id,name,wins"
-	  # * *Raises* :
-	  #   - ++ ->
-	  #
+		#
+		# Removes "__c" from all fields fields
+		# * *Args*    :
+		#   - fields -> comma separated list of fields "id,name,wins__c"
+		# * *Returns* :
+		#   - comma separated list of fields "id,name,wins"
+		# * *Raises* :
+		#   - ++ ->
+		#
 		def self.deforce_fields(fields)
 			fields.downcase.split(',').deforce.join(',')
 		end
 
 	end
-
 end
--- lib/forcifier/json_massager.rb
+++ lib/forcifier/json_massager.rb
@@ -1,16 +1,16 @@
 module Forcifier
 	class JsonMassager
 
-	  def self.deforce_json(json)
-	    ary = []
-	    if json.is_a?(Array)
-	      json.each do |record|
-	        ary << record.deforce_keys!
-	      end
-	      ary
-      elsif json.is_a?(Hash)
-        json.deforce_keys!
-      end
-	  end
+		def self.deforce_json(json)
+			ary = []
+			if json.is_a?(Array)
+				json.each do |record|
+					ary << record.deforce_keys!
+				end
+				ary
+			elsif json.is_a?(Hash)
+				json.deforce_keys!
+			end
+		end
 	end
 end
